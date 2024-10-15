68baf2444ce4f80f300b0c6780f4631739f78069
flinare <143953422+flinare@users.noreply.github.com>
2024-10-15 09:18:36 +0200

    Update EPG_script.sh

--- .github/workflows/EPG_script.sh
+++ .github/workflows/EPG_script.sh
@@ -10,43 +10,43 @@ rm -f EPG_temp*
  		extension="${epg##*.}"
 		if [ $extension = "gz" ]; then
 			echo Descargando y descomprimiendo epg
-			wget -O EPG_temp.xml.gz -q ${epg}
-      			gzip -d -f EPG_temp.xml.gz
+			wget -O EPG_temp00.xml.gz -q ${epg}
+      			gzip -d -f EPG_temp00.xml.gz
 	  	else
 			echo Descargando epg
-			wget -O EPG_temp.xml -q ${epg}
+			wget -O EPG_temp00.xml -q ${epg}
 		fi
+  	cat EPG_temp00.xml >> EPG_temp.xml
 	done < epgs.txt
 
  	while IFS=, read -r old new logo
 	do
 		contar_channel="$(grep -c "channel=\"$old\"" EPG_temp.xml)"
 		if [ $contar_channel -gt 1 ]; then
-
 			sed -n "/<channel id=\"${old}\">/,/<\/channel>/p" EPG_temp.xml > EPG_temp01.xml
-			sed -i '/<display-name/d' EPG_temp01.xml
-			sed -i '/<url/d' EPG_temp01.xml
-
+			sed -i '/<icon src/!d' EPG_temp01.xml
    			if [ "$logo" ]; then
 	 			echo Nombre EPG: $old · Nuevo nombre: $new · Cambiando logo ··· $contar_channel coincidencias
-				sed -i "s#<channel id=.*#<channel id=\"${new}\">#" EPG_temp01.xml
+     				echo '  </channel>' >> EPG_temp01.xml
+	 			sed -i "1i\  <channel id=\"${new}\">" EPG_temp01.xml
 				sed -i "2i\    <display-name>${new}</display-name>" EPG_temp01.xml
     				sed -i "s#<icon src=.*#<icon src=\"${logo}\" \/>#" EPG_temp01.xml
-				sed -i "3i\    <icon src=\"${logo}\" \/>" EPG_temp01.xml
-  				echo '  </channel>' >> EPG_temp01.xml
-      		else
+				sed -i "3i\    <icon src=\"${logo}\" \/>" EPG_temp01.xml  				
+      			else
 				echo Nombre EPG: $old · Nuevo nombre: $new · Manteniendo logo ··· $contar_channel coincidencias
-      				sed -i "s#<channel id=.*#<channel id=\"${new}\">#" EPG_temp01.xml
+    				echo '  </channel>' >> EPG_temp01.xml
+      				sed -i "1i\  <channel id=\"${new}\">" EPG_temp01.xml
 				sed -i "2i\    <display-name>${new}</display-name>" EPG_temp01.xml
-  				echo '  </channel>' >> EPG_temp01.xml
    			fi
       			cat EPG_temp01.xml >> EPG_temp1.xml
 			sed -i '$!N;/^\(.*\)\n\1$/!P;D' EPG_temp1.xml
 
 			sed -n "/<programme.*\"${old}\"/,/<\/programme>/p" EPG_temp.xml > EPG_temp02.xml
-			sed -i "s# channel=\"${old}\"# channel=\"${new}\"#" EPG_temp02.xml
+			sed -i "s# channel=\"${old}\"##g" EPG_temp02.xml		
+			sed -i "/<programme/ a EPG_temp_ channel=\"${new}\">" EPG_temp02.xml
+			sed -i ':a;N;$!ba;s/\nEPG_temp_/EPG_temp_/g' EPG_temp02.xml
+			sed -i "s#>EPG_temp_##g" EPG_temp02.xml
 			cat EPG_temp02.xml >> EPG_temp2.xml
-
 		else
 			echo Saltando canal: $old ··· $contar_channel coincidencias
 		fi
